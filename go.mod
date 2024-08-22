95748a9231efc07f021d9e01127f3617818a96f4
Lissone <leonardo.lissonez@gmail.com>
2024-08-21 21:34:35 -0300

    feat: structured logging

--- go.mod
+++ go.mod
@@ -3,3 +3,10 @@ module go-api
 go 1.23.0
 
 require github.com/go-chi/chi/v5 v5.1.0
+
+require (
+	go.uber.org/zap v1.27.0
+	go.uber.org/zap/exp v0.2.0
+)
+
+require go.uber.org/multierr v1.11.0 // indirect
--- main.go
+++ main.go
@@ -1,15 +1,20 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"errors"
-	"fmt"
 	"io"
+	"log/slog"
 	"net/http"
+	"os"
 	"strconv"
+	"time"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"go.uber.org/zap"
+	"go.uber.org/zap/exp/zapslog"
 )
 
 type Response struct {
@@ -20,14 +25,14 @@ type Response struct {
 func sendJSON(w http.ResponseWriter, resp Response, status int) {
 	data, err := json.Marshal(resp)
 	if err != nil {
-		fmt.Println("error ao fazer marshal de json:", err)
+		slog.Error("error ao fazer marshal de json", "error", err)
 		sendJSON(w, Response{Error: "something went wrong"}, http.StatusInternalServerError)
 		return
 	}
 
 	w.WriteHeader(status)
 	if _, err := w.Write(data); err != nil {
-		fmt.Println("error ao enviar a resposta:", err)
+		slog.Error("error ao enviar a resposta", "error", err)
 		return
 	}
 }
@@ -36,10 +41,59 @@ type User struct {
 	Username string
 	ID       int64 `json:",string"`
 	Role     string
-	Password string `json:"-"`
+	Password Password `json:"-"`
 }
 
+type Password string
+
+func (p Password) String() string {
+	return "[REDACTED]"
+}
+
+func (p Password) LogValue() slog.Value {
+	return slog.StringValue("[REDACTED]")
+}
+
+const LevelFoo = slog.Level(-50)
+
 func main() {
+	z, _ := zap.NewProduction()
+	zs := slog.New(zapslog.NewHandler(z.Core(), nil))
+	zs.Info("Uma mensagem de teste")
+	p := Password("123456")
+	u := User{Password: p}
+	slog.Info("password", "u", u)
+	opts := &slog.HandlerOptions{
+		AddSource: true,
+		Level:     LevelFoo,
+		ReplaceAttr: func(groups []string, a slog.Attr) slog.Attr {
+			if a.Key == "level" {
+				level := a.Value.String()
+				if level == "DEBUG-46" {
+					a.Value = slog.StringValue("FOO")
+				}
+			}
+			return a
+		},
+	}
+	l := slog.New(slog.NewJSONHandler(os.Stdout, opts))
+	slog.SetDefault(l)
+	slog.Debug("foo")
+	slog.Info("Servico sendo iniciado", "version", "1.0.0")
+	l = l.With(slog.Group("app_info", slog.String("version", "1.0.0.")))
+	l.Info("this is a test", "user", u)
+	l.LogAttrs(context.Background(), LevelFoo, "qualquer mensagem")
+	l.LogAttrs(
+		context.Background(),
+		slog.LevelInfo,
+		"tivemos um http request",
+		slog.Group("http_data",
+			slog.String("method", http.MethodDelete),
+			slog.Int("status", http.StatusOK),
+		),
+		slog.Duration("time_taken", time.Second),
+		slog.String("user_agent", "ahsiduas"),
+	)
 	r := chi.NewMux()
 
 	r.Use(middleware.Recoverer)
@@ -102,7 +156,7 @@ func handlePostUsers(db map[int64]User) http.HandlerFunc {
 				return
 			}
 
-			fmt.Println(err)
+			slog.Error("falha ao ler o json do usuario", "error", err)
 			sendJSON(w, Response{Error: "something went wrong"}, http.StatusInternalServerError)
 			return
 		}
